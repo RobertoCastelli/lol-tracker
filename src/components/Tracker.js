ffc5033baa42451522d9081d37d3abea55113e6e
feticcio <feticcio@pop-os.localdomain>
2020-07-19 20:41:43 +0200

    icons

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -1,31 +1,145 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Tracker = () => {
+  //-------
+  //GLOBALS
+  //-------
+  const apiKey = `RGAPI-e00404c1-af9f-48c3-9fa0-9d1d00e98faa`;
+  const encryptedID = `WiRUurtB56WCPODVbWW-_WdGmndH5ZVemP-7_LIf57uNBAE`;
+  const cors = `https://cors-anywhere.herokuapp.com/`;
+  const version = `10.14.1`;
+  const summonerUrl = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name`;
+  const serverUrl = `https://euw1.api.riotgames.com/lol/status/v3/shard-data`;
+  const scoreUrl = `https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner`;
+  const iconUrl = `http://ddragon.leagueoflegends.com/cdn/${version}/img/profileicon`;
+
+  //-----
+  //STATE
+  //-----
+  const [name, setName] = useState("Feticcio");
+  const [url, setUrl] = useState(
+    `${cors}${summonerUrl}/${name}?api_key=${apiKey}`
+  );
+  const [loading, setLoading] = useState(false);
+  const [level, setLevel] = useState("");
+  const [id, setId] = useState(encryptedID);
+  const [icon, setIcon] = useState("");
+  const [status, setStatus] = useState("");
+  const [server, setServer] = useState("");
+  const [wins, setWins] = useState("");
+  const [losses, setLosses] = useState("");
+  const [tier, setTier] = useState("");
+  const [queue, setQueue] = useState("");
+  const [rank, setRank] = useState("");
+
+  //-----
+  //FETCH
+  //-----
+  useEffect(() => {
+    // SUMMONER
+    const fetchSummoner = async () => {
+      setLoading(true);
+      try {
+        const fetchSummoner = await fetch(url);
+        const summonerData = await fetchSummoner.json();
+        console.log(summonerData);
+        setName(summonerData.name);
+        setLevel(summonerData.summonerLevel);
+        setId(summonerData.id);
+        setLoading(false);
+        setIcon(summonerData.profileIconId);
+      } catch (err) {
+        console.log(`there is a problem: ${err.message}`);
+      }
+    };
+
+    // SERVER
+    const fetchServer = async () => {
+      try {
+        const fetchServer = await fetch(
+          `${cors}${serverUrl}?api_key=${apiKey}`
+        );
+        const serverData = await fetchServer.json();
+        console.log(serverData);
+        setStatus(serverData.services[3].status);
+        setServer(serverData.name);
+      } catch (err) {
+        console.log(`there is a problem: ${err.message}`);
+      }
+    };
+
+    // SCORE
+    const fetchScore = async () => {
+      try {
+        const fetchScore = await fetch(
+          `${cors}${scoreUrl}/${id}?api_key=${apiKey}`
+        );
+        const scoreData = await fetchScore.json();
+        console.log(scoreData);
+        setQueue(scoreData[0].queueType);
+        setWins(scoreData[0].wins);
+        setLosses(scoreData[0].losses);
+        setRank(scoreData[0].rank);
+        setTier(scoreData[0].tier);
+      } catch (err) {
+        console.log(`there is a problem: ${err.message}`);
+      }
+    };
+    fetchSummoner();
+    fetchServer();
+    fetchScore();
+  }, [url]);
+
+  //--------
+  //HANDLERS
+  //--------
+  const showLoading = () => (loading ? <h4>Loading...</h4> : "");
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUrl(`${cors}${summonerUrl}/${name}?api_key=${apiKey}`);
+  };
+
+  //------
+  //RENDER
+  //------
   return (
     <div className="card-wrapper">
-      <form>
-        <input type="text" placeholder="search summoner" />
+      <form onSubmit={handleSubmit}>
+        <input
+          value={name}
+          onFocus={(e) => (e.target.value = "")}
+          onChange={handleChange}
+          type="text"
+          placeholder="search summoner"
+        />
         <button type="submit">SEARCH</button>
       </form>
       <fieldset className="card-body">
-        <legend>Name</legend>
+        <legend>{name}</legend>
         <div className="card-info">
           <div className="card-images">
-            <img src="https://via.placeholder.com/100" alt="avatar" />
-            <img src="https://via.placeholder.com/100" alt="icons" />
+            <img src={`${iconUrl}/${icon}.png`} alt="icons" />
+            {/* <img src="https://via.placeholder.com/100" alt="avatar" /> */}
           </div>
+          {showLoading()}
           <ul>
-            <li>rank</li>
-            <li>level</li>
-            <li>score</li>
-            <li>wins</li>
-            <li>losses</li>
+            <li>queue {queue}</li>
+            <li>level {level}</li>
+            <li>
+              tier {tier} {rank}
+            </li>
+            <li>wins {wins}</li>
+            <li>losses {losses}</li>
           </ul>
         </div>
         <div className="card-server">
           <ul>
-            <li>location</li>
-            <li>status</li>
+            <li>region {server}</li>
+            <li>status {status}</li>
           </ul>
         </div>
       </fieldset>
